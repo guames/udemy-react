03db9b2ded24bbeccf8f1699460961924b997c55
games <gustavo.ames87@gmail.com>
2019-05-27 19:13:54 -0700

    class 38
    * Handlers

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state ={
+      persons: [
+          { name: 'Joao', age: 14},
+          { name: 'Pedro', age: 19},
+          { name: 'Paulo', age: 23},
+      ]
+    };
+
+    switchNameHandler = () => {
+        console.log('Was clicked');
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, my 1st React app</h1>
-                <Person name="Joao" age="12"/>
-                <Person name="Pedro" age="18"/>
-                <Person name="Jose" age="22"/>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbie: Surf</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         );
     }
