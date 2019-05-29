19d6f1bbb16ce72d42540f6d2898f4875a9eff42
games <gustavo.ames87@gmail.com>
2019-05-28 19:04:04 -0700

    Section 4 * Conditional

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Person/Person'
 
 const App = props => {
+    const [visibilityPerson, setVisibilityPerson] = useState(true);
     const [personsState, setPersonsState] =  useState({
             persons: [
                 { name: 'Joao', age: 14},
@@ -11,10 +12,6 @@ const App = props => {
             ]
         }
     );
-
-    const [otherState, setOtherState] = useState('Some other value');
-
-    console.log(personsState, otherState);
 
     const switchNameHandler = (newName) => {
         setPersonsState({
@@ -26,17 +23,55 @@ const App = props => {
         });
     };
 
+    const nameChangeHandler = (event) => {
+        setPersonsState({
+            persons: [
+                { name: event.target.value, age: 11},
+                { name: 'Pedro', age: 12},
+                { name: 'Jose', age: 28},
+            ]
+        });
+    };
+
+    const togglePersonHandler = () => {
+        setVisibilityPerson(!visibilityPerson);
+    };
+
+    const style ={
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    };
+
     return (
         <div className="App">
             <h1>Hi, my 1st React app</h1>
-            <button onClick={switchNameHandler.bind(this, 'Gustavo')}>Switch Name</button>
-            <Person click={switchNameHandler.bind(this, 'Joao')}
-                    name={personsState.persons[0].name}
-                    age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name}
+
+            <button
+                style={style}
+                onClick={switchNameHandler.bind(this, 'Gustavo')}>Switch Name</button>
+
+            <button
+                style={style}
+                onClick={togglePersonHandler}>Toggle Visibility</button>
+
+            { visibilityPerson ?
+                <div>
+                    <Person click={switchNameHandler.bind(this, 'Joao')}
+                            name={personsState.persons[0].name}
+                            age={personsState.persons[0].age}
+                            change={nameChangeHandler}/>
+
+                    <Person name={personsState.persons[1].name}
                     age={personsState.persons[1].age}>My hobbie: Surf</Person>
-            <Person name={personsState.persons[2].name}
+                    <Person name={personsState.persons[2].name}
                     age={personsState.persons[2].age}/>
+                </div>
+                : <p>Nothing</p>
+            }
+
         </div>
     );
 };
